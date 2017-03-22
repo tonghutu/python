4a91e218bb6c2d098dee30b45bd73e25d7f75d78
tonghutu <tonghutu@163.com>
2017-03-22 22:44:23 +0800

    Update warm.py

--- warm.py
+++ warm.py
@@ -1,21 +1,53 @@
+# -*- coding:utf-8 -*-
+# coding=UTF-8
 
-import re
-import urllib
+import os,urllib,urllib2,re
+
+url = u"http://www.zngirls.com/g/21498/8.html"
+outpath = "/Users/dufei/Code/Python/warm/"
 
 def getHtml(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    return html
-def getImg(html):
-    reg = r'src="(.*?\.jpg)" pic_ext=' #此处为正则表达式，用于匹配页面中的图片链接，不同的网页图片链接的格式有所不用，此处需按需调整
-    imgre = re.compile(reg)
-    imglist = re.findall(imgre,html)
+    webfile = urllib.urlopen(url)
+    outhtml = webfile.read()
+    print outhtml
+    return outhtml
+
+def getImageList(html):
+    restr=ur'('
+    restr+=ur'http:\/\/[^\s,"]*\.jpg'
+    restr+=ur'|http:\/\/[^\s,"]*\.jpeg'
+    restr+=ur'|http:\/\/[^\s,"]*\.png'
+    restr+=ur'|http:\/\/[^\s,"]*\.gif'
+    restr+=ur'|http:\/\/[^\s,"]*\.bmp'
+    restr+=ur'|https:\/\/[^\s,"]*\.jpeg'
+    restr+=ur'|https:\/\/[^\s,"]*\.jpeg'
+    restr+=ur'|https:\/\/[^\s,"]*\.png'
+    restr+=ur'|https:\/\/[^\s,"]*\.gif'
+    restr+=ur'|https:\/\/[^\s,"]*\.bmp'
+    restr+=ur')'
+    htmlurl = re.compile(restr)
+    imgList = re.findall(htmlurl,html)
+    print imgList
+    return imgList
+
+def download(imgList, page):
     x = 1
-    for imgurl in imglist:
-        urllib.urlretrieve(imgurl,'%s.gif' % x )
-        print "第",x,"张下载完成！"
+    for imgurl in imgList:
+        filepathname=str(outpath+'pic_%09d_%010d'%(page,x)+str(os.path.splitext(urllib2.unquote(imgurl).decode('utf8').split('/')[-1])[1])).lower()
+        print '[Debug] Download file :'+ imgurl+' >> '+filepathname
+        urllib.urlretrieve(imgurl,filepathname)
+        if os.path.getsize(filepathname) < 102400:
+            os.remove(filepathname)
         x+=1
-#将图片所在网页链接放入此处，大部分在WINDOWS下使用，所以不采用变量传入的方式        
-html = getHtml("http://tieba.baidu.com/p/3191208081")
-#print html
-getImg(html)
+
+def downImageNum(pagenum):
+    page = 1
+    pageNumber = pagenum
+    while(page <= pageNumber):
+        html = getHtml(url)#获得url指向的html内容
+        imageList = getImageList(html)#获得所有图片的地址，返回列表
+        download(imageList,page)#下载所有的图片
+        page = page+1
+
+if __name__ == '__main__':
+    downImageNum(1)
